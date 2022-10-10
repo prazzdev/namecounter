b4ec49002180639f77d51da4bd38a68ac8adcaa6
Agung Prasetyo <prazzid31@gmail.com>
2022-10-10 23:26:30 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+    "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+]
+}
